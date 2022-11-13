0726bbfc6af66b896376768890099ba2cfcff0f7
AchmadMahfud26 <achmadmahfud01@gmail.com>
2022-11-13 09:45:47 +0700

    1

---
+++ KiosPetani2/app/src/main/java/com/kiospetani/MainActivity.java
@@ -0,0 +1,19 @@
+package com.kiospetani;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
